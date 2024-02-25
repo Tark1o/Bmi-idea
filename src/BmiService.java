7ab09c7c6a2c745bd01497fb22769c99422cd0cc
Tarkio <wowsendarpm@gmail.com>
2024-02-25 21:33:37 +0300

    gitignore

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public double calculate(double height, int weight) {
+    public long calculate(double height, int weight) {
         long calculate = (long) (weight / (height * height));
         return calculate;
     }
